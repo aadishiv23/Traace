62dd965bd64451c4c7c9b3180ab6973b01344214
aadishiv <aadishiv@outlook.com>
2025-01-29 11:04:31 -0500

    init

---
+++ Plore/PloreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PloreApp.swift
+//  Plore
+//
+//  Created by Aadi Shiv Malhotra on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PloreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PloreTests/PloreTests.swift
@@ -0,0 +1,17 @@
+//
+//  PloreTests.swift
+//  PloreTests
+//
+//  Created by Aadi Shiv Malhotra on 1/29/25.
+//
+
+import Testing
+@testable import Plore
+
+struct PloreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
